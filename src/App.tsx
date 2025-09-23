977c8ec83058d64c7c2de4cd03d6e580d5fe5f3c
Sathyanarayanan V <dev.sathya1701@gmail.com>
2025-09-23 16:28:32 +0530

    (chore):initial project setup --install and configure tailwind --add basic styles --add font

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-amber-500 text-white">
+      <h1 className="text-4xl">Queue Pilot</h1>
+    </div>
+  );
+}
+
+export default App;
